49c41a305b10f81552435590fb068ec39379007e
Arnold Lei <arnold.lei88@gmail.com>
2016-08-07 18:26:40 -0400

    setting up all dependencies and the database

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chickpea Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', false);
---
+++ server.js
@@ -0,0 +1,4 @@
+var express     = require('express');
+var method      = require('method-override');
+var body        = require('body-parser');
+var mysql       = require('mysql');
