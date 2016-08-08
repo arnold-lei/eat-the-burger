e9476d0e0db9a7f4d804434d8c33771c542908b0
Arnold Lei <arnold.lei88@gmail.com>
2016-08-08 15:24:24 -0400

    getting the server up and running

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-var express     = require('express');
-var method      = require('method-override');
-var body        = require('body-parser');
-var mysql       = require('mysql');
+//App depencencies -----------------------------------------/
+var express         = require('express');
+var bodyParser      = require('body-parser');
+var exphbs          = require('express-handlebars');
+var methodOverride  = require('method-override');
+var app             = express();
+var connection      = require('./config/connection')
+var PORT            = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.listen(PORT);
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+//ROUTES!!!!!!
+app.get('/', function(req, res){
+    res.send("Welcome to the Hamburger Page!")
+});
