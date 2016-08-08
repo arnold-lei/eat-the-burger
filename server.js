6c354abc111d2bac0262cd23e0e3070d31f6f53d
Arnold Lei <arnold.lei88@gmail.com>
2016-08-08 15:46:52 -0400

    working on handlebar templates

--- server.js
+++ server.js
@@ -7,21 +7,17 @@ var app             = express();
 var connection      = require('./config/connection')
 var PORT            = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+//Handlebars configureation
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.listen(PORT);
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
+
 
 //ROUTES!!!!!!
 app.get('/', function(req, res){
-    res.send("Welcome to the Hamburger Page!")
+    res.render('index');
 });
+
+
+
+app.listen(PORT);
