f603a541969c5eaa475fcf0a80bac38d91920834
Arnold Lei <arnold.lei88@gmail.com>
2016-08-09 18:34:14 -0400

    Got handlebars to list out burgers from MySQL

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var bodyParser      = require('body-parser');
 var exphbs          = require('express-handlebars');
 var methodOverride  = require('method-override');
 var app             = express();
-var connection      = require('./config/connection')
+var connection      = require('./config/connection');
 var PORT            = 3000;
+var orm             = require('./config/orm');
 
 //Handlebars configureation
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -15,9 +16,20 @@ app.set('view engine', 'handlebars');
 
 //ROUTES!!!!!!
 app.get('/', function(req, res){
-    res.render('index');
+    connection.query('SELECT * FROM burgers;', function(err,rows, data){
+        res.render('index', {rows: rows});
+    })
 });
 
+app.get('/api', function(req, res){
+    connection.query('SELECT * FROM burgers;', function(err, data){
+        if (err) throw err;
+        res.send(data);
+    })
+})
 
+app.get('/api/test', function(req, res){
+
+})
 
 app.listen(PORT);
